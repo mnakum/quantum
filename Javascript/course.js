ead5dacb18d8dff7a08d62b4ca8aaa836edd97e6
Meet Nakum <meet.nakum@zeuslearning.com>
2023-01-20 10:03:36 +0530

    made changes accoriding feedback

--- Javascript/course.js
+++ Javascript/course.js
@@ -1,29 +1,18 @@
-// const courses = require("../Json/courses.json");
-const courses = [];
 fetch("../Json/courses.json")
   .then((response) => {
     return response.json();
   })
-  .then((data) => {
-    data.map((d) => {
-      courses.push(d);
-    });
+  .then((courses) => {
     const container = document.getElementById("container");
 
-    courses.forEach((result, idx) => {
-      let disabled = result.classes === "No Classes" ? "disabled" : "";
-      let btwnclass = result.managecourse ? "btwn" : "btwn-dis";
-      console.log(btwnclass);
-
+    courses.forEach((result) => {
       const content = `
       <div class="card">
-          ${
-            result.expired ? `<div class="expired" > <p> EXPIRED</p></div>` : ``
-          }
+        ${result.expired ? `<div class="expired" > <p> EXPIRED</p></div>` : ``}
           <div class="content" id="stuff">
             <img src=" ${result.img} " alt="img1">
             <div class="info">
-              <h3>${result.course}</h3>
+              <p class= "coursename">${result.course}</p>
               <p>${
                 result.subject
               } <span>&nbsp&nbsp&nbsp|&nbsp&nbsp&nbsp</span>${result.grade}</p>
@@ -34,37 +23,55 @@ fetch("../Json/courses.json")
       }</b>&nbspLessons <b style="color: #222222;">${
         result.topics
       }</b>&nbspTopics   </p>
-              <select name="sorting" id="sorting" ${disabled} >
-                <option value="name">${result.classes}</option>
-              </select>
-              <p>50 Students<span>&nbsp&nbsp&nbsp|&nbsp&nbsp&nbsp</span>${
-                result.dates
+                ${
+                  result.classes.length === 0
+                    ? `<select name="sorting" id="sorting" disabled style="opacity:0.4" >
+                      <option value="name">No Classes</option>
+                    </select>`
+                    : `<select name="sorting" id="sorting">
+                      ${result.classes.map((c) => {
+                        return `<option value="name">${c}</option>`;
+                      })}
+                    </select>`
+                }
+              <p>50 Students${
+                result.startdates === ""
+                  ? ``
+                  : `<span>&nbsp&nbsp&nbsp|&nbsp&nbsp&nbsp</span>` +
+                    result.startdates +
+                    "-" +
+                    result.enddates
               }</p>
             </div>
             <div class="favourite">
-              <img src="./assets/icons/favourite.svg" alt="">
-            </div>
+              <button id="favbtn${
+                result.id
+              }" class="deactive" onclick=fav(favbtn${result.id}) ></button>
+            </div>  
             </div>
           <div class="cardbtns">
             <a href="#">
-              <img src="./assets/icons/preview.svg" alt="preview" />
+              <img src="/assets/icons/preview.svg" alt="preview" ${
+                result.previewEnabled ? `` : `class =btwn-dis`
+              } />
             </a>
             <a href="#">
               <img
-                class=${btwnclass}
-                src="./assets/icons/manage_course.svg"
+                ${result.managecourseEnabled ? `` : `class =btwn-dis`}
+                src="/assets/icons/manage_course.svg"
                 alt="manage"
               />
             </a>
             <a href="#">
               <img
-                class=${btwnclass}
-                src="./assets/icons/grade_submissions.svg"
+                ${result.gradesubmissionEnabled ? `` : `class =btwn-dis`}
+                src="/assets/icons/grade_submissions.svg"
                 alt="grade"
               />
             </a>
             <a href="#">
-              <img src="./assets/icons/reports.svg" alt="report" />
+              <img src="/assets/icons/reports.svg" alt="report" 
+              ${result.reportcourseEnabled ? `` : `class =btwn-dis`}/>
             </a>
           </div>
         </div>
